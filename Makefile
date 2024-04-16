904c07fe663267baadb3cc2415e51741ebf552d5
Ben Mattes Krusekamp <ben.krause05@gmail.com>
2024-04-16 09:28:27 +0200

    try to link lua

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 SOURCE_FILES=$(wildcard *.cpp *.hpp)
 
 all:
-	$(CXX) -shared -fPIC $(SOURCE_FILES)  -o hyprLua.so -g `pkg-config --cflags pixman-1 libdrm hyprland` -std=c++2b
+	$(CXX) -shared -fPIC $(SOURCE_FILES)  -o hyprLua.so -I/usr/include/lua5.3 -llua5.3 -g `pkg-config --cflags pixman-1 libdrm hyprland` -std=c++2b
--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <hyprland/src/plugins/PluginAPI.hpp>
 
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+
 inline HANDLE PHANDLE = nullptr;
 
 // Do NOT change this function.
@@ -7,11 +11,20 @@ APICALL EXPORT std::string PLUGIN_API_VERSION() {
     return HYPRLAND_API_VERSION;
 }
 
+void init(){
+    lua_State *state = luaL_newstate();
+    luaL_openlibs(state);
+
+    //if (luaL_dofile(state, "/home/main/.config/hypr/init.lua") == LUA_OK) {
+    //    lua_pop(state, lua_gettop(state));
+    //}
+
+
+    lua_close(state);
+}
 APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle) {
     PHANDLE = handle;
-
     const std::string HASH = __hyprland_api_get_hash();
-
     // ALWAYS add this to your plugins. It will prevent random crashes coming from
     // mismatched header versions.
     if (HASH != GIT_COMMIT_HASH) {
@@ -19,6 +32,8 @@ APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle) {
                                      CColor{1.0, 0.2, 0.2, 1.0}, 5000);
         throw std::runtime_error("HyprLua Version mismatch");
     }
+
+    init();
 
     return {"HyprLua", "Lua integration for Hyprland API", "Luposmi", "0.0"};
 }
